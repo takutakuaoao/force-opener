2106e16ac209358b9c6febcf075f015659aa86a5
aochan <nakinagaraodoru1013@gmail.com>
2024-10-08 15:49:38 +0000

    On php-cs-fixer.dist, setting align rules.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,10 +6,10 @@ $config = new PhpCsFixer\Config();
 
 return $config
     ->setRules([
-        '@Symfony' => true,
-        '@PSR12' => true,
-        'concat_space' => ['spacing' => 'one'],
+        '@Symfony'                => true,
+        '@PSR12'                  => true,
+        'concat_space'            => ['spacing' => 'one'],
         'function_typehint_space' => true,
-        'binary_operator_spaces' => true,
-        'php_unit_method_casing' => ['case' => 'snake_case'],
+        'binary_operator_spaces'  => ['operators' => ['=>' => 'align', '=' => 'align']],
+        'php_unit_method_casing'  => ['case' => 'snake_case'],
     ]);
