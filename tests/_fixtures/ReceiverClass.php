ad4122c066fb42336bc19762807c993a7905e8aa
aochan <nakinagaraodoru1013@gmail.com>
2024-10-09 00:26:21 +0000

    Added tests accessing to basic type on the receiver.

--- tests/_fixtures/ReceiverClass.php
+++ tests/_fixtures/ReceiverClass.php
@@ -7,4 +7,18 @@ namespace Tests\Fixtures;
 class ReceiverClass
 {
     public string $publicStringProperty = 'public-string-property';
+    public int $publicIntProperty       = 100;
+    public bool $publicBoolProperty     = true;
+    /** @var array<string> */
+    public array $publicArrayProperty      = ['list_1'];
+    public ?string $publicNullableProperty = null;
+
+    public object $publicNestObjectProperty;
+
+    public function __construct()
+    {
+        $this->publicNestObjectProperty = new class () {
+            public string $publicProperty = 'nest-object-public-property-value';
+        };
+    }
 }
--- tests/units/ForceOpenerTest.php
+++ tests/units/ForceOpenerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Units;
 
 use ForceOpener\ForceOpener;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Tests\Fixtures\ReceiverClass;
 
@@ -15,11 +16,50 @@ class ForceOpenerTest extends TestCase
         $this->receiver = new ReceiverClass();
     }
 
-    public function test_access_public_property(): void
+    #[DataProvider('dataProviderPublicProperties')]
+    public function test_access_public_property(string $propertyName, mixed $expected): void
     {
         $forceOpener = new ForceOpener($this->receiver);
 
-        $this->assertEquals('public-string-property', $forceOpener->publicStringProperty);
+        $this->assertEquals($expected, $forceOpener->$propertyName);
+    }
+
+    /**
+     * @return array<string, array<mixed>>
+     */
+    public static function dataProviderPublicProperties(): array
+    {
+        return [
+            'string' => [
+                'propertyName' => 'publicStringProperty',
+                'expected'     => 'public-string-property',
+            ],
+            'int' => [
+                'propertyName' => 'publicIntProperty',
+                'expected'     => 100,
+            ],
+            'bool' => [
+                'propertyName' => 'publicBoolProperty',
+                'expected'     => true,
+            ],
+            'array' => [
+                'propertyName' => 'publicArrayProperty',
+                'expected'     => ['list_1'],
+            ],
+            'null' => [
+                'propertyName' => 'publicNullableProperty',
+                'expected'     => null,
+            ],
+        ];
+    }
+
+    public function test_access_nest_object_public_property(): void
+    {
+        $forceOperator = new ForceOpener($this->receiver);
+
+        $nestedObject = (object) $forceOperator->publicNestObjectProperty;
+
+        $this->assertEquals('nest-object-public-property-value', $nestedObject->publicProperty);
     }
 
     public function test_raise_error_if_access_non_existent_property(): void
